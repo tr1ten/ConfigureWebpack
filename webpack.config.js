a7603660251f8c8170f0a98ddb3b60a0367fae04
tri10 <shubhi.blog@gmail.com>
2021-11-28 19:36:53 +0530

    adding comments for future references

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,23 @@
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // for extracting css in seperate file
+const HtmlWebpackPlugin = require("html-webpack-plugin"); // automatically adding index.html file in final output
+const {CleanWebpackPlugin} = require("clean-webpack-plugin"); // removing old build output directory 
 const path = require("path");
 module.exports = {
   mode: "development",
-  devtool: false,
+  devtool: false, // just to make js more readable??
   output: {
-    path: path.resolve(__dirname, "dist"),
-    assetModuleFilename: "images/[hash][ext][query]",
+    path: path.resolve(__dirname, "dist"), // to make it know the output build abs path for clean
+    assetModuleFilename: "images/[hash][ext][query]", // for images to put them in seperate dir
   },
   module: {
+    // all the rules for different files
     rules: [
+      // images
       {
         test: /\.(png|jpe?g|svg|gif)/,
         type: "asset",
       },
+      // js/jsx files
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
@@ -22,13 +25,15 @@ module.exports = {
           loader: "babel-loader",
         },
       },
+      // sass scss css
       {
         test: /\.(s[ac]|c)ss$/i,
         use: [
           {
             loader: MiniCssExtractPlugin.loader,
-            options: { publicPath: "" },
+            options: { publicPath: "" }, // idk just fix minicss error 
           },
+          // order matter last to first 
           "css-loader",
           "postcss-loader",
           "sass-loader",
@@ -36,9 +41,11 @@ module.exports = {
       },
     ],
   },
+  // to resolve test.jsx test.js from import test 
   resolve: {
     extensions: [".js", ".jsx"],
   },
+  // plugins used by webpack
   plugins: [
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(),
@@ -46,6 +53,7 @@ module.exports = {
       template: "./src/index.html",
     }),
   ],
+  // dev server stuff 
   devServer: {
     static: "./dist",
     hot: true,
